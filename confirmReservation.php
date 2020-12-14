11a03304b9e13486b845055ec3465a2f9a672280
JeffKwakou <jeff.gbeho@gmail.com>
2020-12-14 18:57:51 +0100

    Ajout de la pré-réservation

---
+++ confirmReservation.php
@@ -0,0 +1,47 @@
+<?php 
+    session_start();
+
+    //Connexion à la database
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=viking_park;charset=utf8', 'root', 'root');
+    } 
+    catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    //Déclaration des variables
+    $prixEnfant = $_POST['billetEnfant'];
+    $prixAdulte = $_POST['billetAdulte'];
+    $prixFamille = $_POST['billetFamille'];
+
+    $nbEnfant = $_POST['nbEnfant'];
+    $nbAdulte = $_POST['nbAdulte'];
+    $nbFamille = $_POST['nbFamille'];
+
+    $date = date("d/m/Y", strtotime($_POST['date']));
+    $currentDate = date("d/m/Y");
+
+    //Vérifier que l'utilisateur est connecté
+    if (isset($_SESSION['id'])) {
+        //Retour à la page si les trois prix sont à 0
+        if ($prixEnfant * $nbEnfant == 0 AND $prixAdulte * $nbAdulte == 0 AND $prixFamille * $nbFamille == 0) {
+            header('Location: reservation.phtml?reservation=you');
+        } else {
+            if ( $currentDate <= $date) {
+                //Calculer le prix total de chaque catégorie
+                $resultEnfant = $prixEnfant * $nbEnfant;
+                $resultAdulte = $prixAdulte * $nbAdulte;
+                $resultFamille = $prixFamille * $nbFamille;
+                $total = $resultAdulte + $resultEnfant + $resultFamille;
+
+                include 'confirmReservation.phtml';
+            } else {
+                //Retour à la page précédente
+                header('Location: reservation.phtml?date=error');
+            }            
+        }
+    } else {
+        //Retour à la page précédente
+        header('Location: reservation.phtml?reservation=error');
+    }
+?>
---
+++ saveReservation.php
@@ -0,0 +1,50 @@
+<?php 
+    session_start();
+
+    //Connexion à la database
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=viking_park;charset=utf8', 'root', 'root');
+    } 
+    catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    //Déclaration des variables
+    $date = $_GET['date'];
+    $quantity = $_GET['quantity'];
+
+    //Vérifier le paramètre de l'url et si l'utilisateur est bien connecté
+    if (isset($_GET['quantity']) != 0 AND isset($_SESSION['id'])) {
+        //Vérifier qu'il y a pas 500 tickets vendus à la date de réservation 
+        $reqQuantityTickets = $database->prepare("SELECT SUM(quantity) AS totalTicket FROM reservationticket WHERE date_reservation = ?");
+        $reqQuantityTickets->execute(array($date));
+        $quantityTickets = $reqQuantityTickets->fetch();
+        if ($quantityTickets['totalTicket'] + (int)$quantity <= 500 OR $quantityTickets == null) {
+            //Générer un code de billet
+            $code = "";
+            for($i = 0; $i < 10; $i++) {
+                $random = strval(random_int(0,9));
+                $code .= $random;
+            }
+
+            //Insérer le ticket dans la database
+            $reqSaveTicket = $database->prepare("INSERT INTO reservationticket (code, quantity, date_reservation, Id_User) VALUES (:code, :quantity, :date_reservation, :Id_User)");
+            $reqSaveTicket->execute(array(
+                'code' => $code,
+                'quantity' => intval($quantity),
+                'date_reservation' => $date,
+                'Id_User' => $_SESSION['id']
+            ));
+
+            //Retour à la page précédente
+            header('Location: reservation.phtml?reservation=success');
+            
+        } else {
+            //Retour à la page précédente
+            header('Location: reservation.phtml?quantityticket=error');
+        }
+    } else {
+        //Retour à la page précédente
+        header('Location: reservation.phtml?reservation=error');
+    }
+?>
